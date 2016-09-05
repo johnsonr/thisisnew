4cf7123528a8b4c779ddf455478b262dd31a2ec7
atomist <admin@atomist.com>
2016-09-05 00:26:47 -0700

    Initial commit from the atomist bot

---
+++ src/main/java/org/johnsonr/ThisIsNewConfiguration.java
@@ -0,0 +1,9 @@
+package org.johnsonr;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ThisIsNewConfiguration {
+    // Your beans go here ...
+    // Atomist Bot suggests "maybe use an Atomist project editor to add a sample one?"
+}
